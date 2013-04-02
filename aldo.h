e886ace30faa8e17e21a108793d129721463f2f0
Aldo Rey Balagulan <mrawesomer34@gmail.com>
2013-04-03 07:57:38 +0800

    Initial commit

---
+++ aldo.h
@@ -0,0 +1,19 @@
+/*
+ * aldo.h
+ *
+ * A custom header files containing my custom functions and shit
+ */
+
+void clearscreen() {
+    /**
+     * A function that detects the running operating system and makes the
+     * necessary function calls to clear the screen.
+     */
+    #if defined(__WIN32__)
+        system("cls");
+    #endif
+
+    #if defined(__linux__)
+        system("clear");
+    #endif
+}
---
+++ array-sort.c
@@ -0,0 +1,25 @@
+/**
+ * array-sort.c
+ *
+ * A small CLI program that simulates three array sorting algorithms, while
+ * showing each step visually
+ *
+ * Written by Aldo Rey Balagulan and Katherine Rollo
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "aldo.h"
+
+int main() {
+    char opt;
+
+    printf("Hello world!\n");
+    printf("Press enter key to go clear the screen\n");
+    getchar();
+
+    clearscreen();
+
+    return 0;
+}
